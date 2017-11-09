b9ff5e53d054ceed617649d59cccb9c5b976bec2
Ydobon73 <32130524+Ydobon73@users.noreply.github.com>
2017-11-09 12:22:31 +0300

    <kzzzzz

--- WebServer.java
+++ WebServer.java
@@ -1,9 +1,6 @@
 import com.sun.net.httpserver.*;
 import java.io.*;
 import java.net.InetSocketAddress;
-import java.text.DateFormat;
-import java.util.Date;
-import java.util.TimeZone;
 
 import static javax.imageio.ImageIO.read;
 
@@ -33,31 +30,11 @@ public class WebServer extends Thread {
                 // возвращаем "400 Bad Request"
                 if (path == null) {
                     // первая строка ответа
-                    String response = "HTTP/1.1 400 Bad Request\n";
-
-                    // дата в GMT
-                    DateFormat df = DateFormat.getTimeInstance();
-                    df.setTimeZone(TimeZone.getTimeZone("GMT"));
-                    response = response + "Date: " + df.format(new Date()) + "\n";
-
-                    // остальные заголовки
-                    response = response
-                            + "Connection: close\n"
-                            + "Server: SimpleWEBServer\n"
-                            + "Pragma: no-cache\n\n";
-
-                    // выводим данные:
-                    exchange.sendResponseHeaders(200, response.length());
+                    String response = "400 Bad Request\n";
+                    exchange.sendResponseHeaders(200, 0);
                     OutputStream os = exchange.getResponseBody();
                     os.write(response.getBytes());
                     os.close();
-
-                    //os.write(response.getBytes());
-
-                    // завершаем соединение
-                    os.close();
-
-                    // выход
                     return;
                 }
 
@@ -78,102 +55,31 @@ public class WebServer extends Thread {
                 // если по указанному пути файл не найден
                 // то выводим ошибку "404 Not Found"
                 if (flag) {
-                    // первая строка ответа
-                    String response = "HTTP/1.1 404 Not Found\n";
+                    String response = "404 Not Found\n";
 
-                    // дата в GMT
-                    DateFormat df = DateFormat.getTimeInstance();
-                    df.setTimeZone(TimeZone.getTimeZone("GMT"));
-                    response = response + "Date: " + df.format(new Date()) + "\n";
-
-                    // остальные заголовки
-                    response = response
-                            + "Content-Type: text/plain\n"
-                            + "Connection: close\n"
-                            + "Server: SimpleWEBServer\n"
-                            + "Pragma: no-cache\n\n";
-
-                    // и гневное сообщение
                     response = response + "File " + path + " not found!";
 
-                    exchange.sendResponseHeaders(200, response.length());
+                    exchange.sendResponseHeaders(200, 0);
                     OutputStream os = exchange.getResponseBody();
                     os.write(response.getBytes());
                     os.close();
-                   // выводим данные:
                     os.write(response.getBytes());
-
-                    // завершаем соединение
                     os.close();
-
-                    // выход
                     return;
-                }
-
-                // определяем MIME файла по расширению
-                // MIME по умолчанию - "text/plain"
-                String mime = "text/plain";
-
-                // выделяем у файла расширение (по точке)
-                int r = path.lastIndexOf(".");
-                if (r > 0) {
-                    String ext = path.substring(r);
-                    if (ext.equalsIgnoreCase("html"))
-                        mime = "text/html";
-                    else if (ext.equalsIgnoreCase("htm"))
-                        mime = "text/html";
-                    else if (ext.equalsIgnoreCase("gif"))
-                        mime = "image/gif";
-                    else if (ext.equalsIgnoreCase("jpg"))
-                        mime = "image/jpeg";
-                    else if (ext.equalsIgnoreCase("jpeg"))
-                        mime = "image/jpeg";
-                    else if (ext.equalsIgnoreCase("bmp"))
-                        mime = "image/x-xbitmap";
                 }
 
                 // создаём ответ
 
-                // первая строка ответа
-                String response = "HTTP/1.1 200 OK\n";
-
-                // дата создания в GMT
-                DateFormat df = DateFormat.getTimeInstance();
-                df.setTimeZone(TimeZone.getTimeZone("GMT"));
-
-                // время последней модификации файла в GMT
-                response = response + "Last-Modified: " + df.format(new Date(f.lastModified())) + "\n";
-
-                // длина файла
-                response = response + "Content-Length: " + f.length() + "\n";
-
-                // строка с MIME кодировкой
-                response = response + "Content-Type: " + mime + "\n";
-
-                // остальные заголовки
-                response = response
-                        + "Connection: close\n"
-                        + "Server: SimpleWEBServer\n\n";
-
-                // выводим заголовок:
-                exchange.sendResponseHeaders(200, response.length());
+                exchange.sendResponseHeaders(200, 0);
                 OutputStream os = exchange.getResponseBody();
-                os.write(response.getBytes());
-
-                /*os.close();
-                os.write(response.getBytes());*/
-
-                // и сам файл:
                 byte buf[] = new byte[64 * 1024];
                 FileInputStream fis = new FileInputStream(path);
-                r = 1;
+                int r = 1;
                 while (r > 0) {
                     r = fis.read(buf);
                     if (r > 0) os.write(buf, 0, r);
                 }
                 fis.close();
-
-                // завершаем соединение
                 os.close();
             } catch (Exception e) {
                 e.printStackTrace();
@@ -181,24 +87,15 @@ public class WebServer extends Thread {
         }
 
 
-        // "вырезает" из HTTP заголовка URI ресурса и конвертирует его в filepath
-        // URI берётся только для GET и POST запросов, иначе возвращается null
         protected String getPath(String request) {
-            // ищем URI, указанный в HTTP запросе
-            // URI ищется только для методов POST и GET, иначе возвращается null
             String  path;
             if (request == null) return null;
-
-            // если URI записан вместе с именем протокола
-            // то удаляем протокол и имя хоста
             path = request.toLowerCase();
             if (path.indexOf("http://", 0) == 0) {
                 request = request.substring(7);
                 request = request.substring(request.indexOf("/", 0));
             } else if (path.indexOf("/", 0) == 0)
                 request = request.substring(1); // если URI начинается с символа /, удаляем его
-
-
 
             // конвертируем URI в путь до документов
             // предполагается, что документы лежат в папке C://www/
